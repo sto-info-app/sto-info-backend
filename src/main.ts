5bbe02d2a7f107c14356bec4193df293a523da83
Steve Roberts <steve@shadowcomputers.co.uk>
2023-05-08 17:10:44 +0100

    Set up swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Set up Swagger
+  const config = new DocumentBuilder()
+    .setTitle('STO Info API')
+    .setDescription('The STO Info API documentation')
+    .setVersion('1.0')
+    .addBearerAuth() // Enable JWT authentication in Swagger UI
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(parseInt(process.env.APP_PORT) || 3000);
 }
 bootstrap();
