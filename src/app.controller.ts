0fc7659aed1bc7800cca8369035e826ed2c5dda5
Steve Roberts <steve@shadowcomputers.co.uk>
2023-05-08 19:12:11 +0100

    Added swagger documentation to default API

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
+@ApiTags('Core APIs')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get Hello plus the environment name' })
+  @ApiResponse({
+    status: 200,
+    description: 'The hello message has been successfully returned.',
+  })
   getHello(): string {
     return this.appService.getHello();
   }
