28c626a814e2b2b00e7ed586a095fafea1aa0c8a
Steve Roberts <steve@shadowcomputers.co.uk>
2023-05-08 17:46:20 +0100

    Added database TypeORM connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,28 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      isGlobal: true,
       envFilePath: `config/environments/${process.env.NODE_ENV || ''}.env`,
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as any,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      schema: process.env.DB_SCHEMA,
+      synchronize: process.env.TYPEORM_SYNCHRONIZE === 'true',
+      logging: process.env.TYPEORM_LOGGING === 'true',
+      entities: [join(__dirname, process.env.TYPEORM_ENTITIES)],
+      migrations: [join(__dirname, process.env.TYPEORM_MIGRATIONS)],
     }),
   ],
   controllers: [AppController],
