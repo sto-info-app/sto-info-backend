a33b26b037fee46b3b75c6cdee877b187fccc4b1
Steve Roberts <steve@shadowcomputers.co.uk>
2023-05-11 02:57:26 +0100

    Update last login date when user logs in

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as bcrypt from 'bcrypt';
 import { User } from 'src/user/entities/user.entity';
+import { UserService } from 'src/user/user.service';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -11,6 +12,7 @@ export class AuthService {
     @InjectRepository(User)
     private userRepository: Repository<User>,
     private jwtService: JwtService,
+    private userService: UserService,
   ) {}
 
   async register(user: Partial<User>): Promise<User> {
@@ -22,10 +24,16 @@ export class AuthService {
     return this.userRepository.save(newUser);
   }
 
-  async validateUser(email: string, password: string): Promise<User | null> {
-    const user = await this.userRepository.findOne({ where: { email: email } });
-    if (user && (await bcrypt.compare(password, user.password))) {
-      return user;
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.userService.findByEmail(email);
+    if (user && (await user.comparePassword(password))) {
+      // Update last login time
+      user.lastLogin = new Date();
+      await this.userService.update(user.id, user);
+
+      // Remove password from user object before returning it
+      const { password, ...result } = user;
+      return result;
     }
     return null;
   }
