c04a32a820a1379d566dfdac95bc1bce0d77b4d1
Steve Roberts <steve@shadowcomputers.co.uk>
2023-05-14 16:47:30 +0100

    Added rate limiting (and commenting!)

--- src/main.ts
+++ src/main.ts
@@ -5,22 +5,35 @@ import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.enableCors();
-  app.use(helmet());
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  const rateLimit = require('express-rate-limit'); // Import rate limiting module
 
-  // Set up Swagger
+  // Define rate limiting rules
+  const apiLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // Rate limiting window set to 15 minutes
+    max: 100, // Maximum number of requests per IP within the window
+  });
+
+  // Create NestJS application
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ transform: true })); // Enable data validation with transform option
+  app.enableCors(); // Enable CORS (Cross-Origin Resource Sharing)
+  app.use(helmet()); // Enable Helmet, a collection of 11 smaller middleware functions that set security-related HTTP headers
+  app.use('/', apiLimiter); // Apply rate limiting to all routes
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector))); // Enable class serializer interceptor for managing response data
+
+  // Set up Swagger for API documentation
   const config = new DocumentBuilder()
     .setTitle('STO Info API')
     .setDescription('The STO Info API documentation')
     .setVersion('1.0')
     .addBearerAuth() // Enable JWT authentication in Swagger UI
     .build();
+
+  // Set up Swagger UI endpoint
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
+  // Start listening for requests on the specified port
   await app.listen(parseInt(process.env.APP_PORT) || 3000);
 }
 bootstrap();
