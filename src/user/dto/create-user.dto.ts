b3d637a0d44546cefc74797bd2986e7b9474eb8c
Steve Roberts <steve@shadowcomputers.co.uk>
2023-05-31 01:23:30 +0100

    Fixed registration validation

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,13 +1,28 @@
-import { Exclude } from 'class-transformer';
-import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  readonly firstName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly lastName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(5)
+  readonly username: string;
+
   @IsNotEmpty()
   @IsEmail()
   readonly email: string;
 
-  @Exclude()
   @IsNotEmpty()
   @MinLength(8)
   readonly password: string;
+
+  @IsNotEmpty()
+  @MinLength(8)
+  readonly confirmPassword: string;
 }
