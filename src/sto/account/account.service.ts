fb497feb70c56f168dd04c7a247125f28d1c37a1
Steve Roberts <steve@shadowcomputers.co.uk>
2023-06-12 18:09:32 +0100

    Added STO account endpoint and functions

--- src/sto/account/account.service.ts
+++ src/sto/account/account.service.ts
@@ -1,26 +1,85 @@
-import { Injectable } from '@nestjs/common';
-// import { CreateAccountDto } from './dto/create-account.dto';
-// import { UpdateAccountDto } from './dto/update-account.dto';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { LessThan, Repository } from 'typeorm';
+
+import { CreateAccountDto } from './dto/create-account.dto';
+import { UpdateAccountDto } from './dto/update-account.dto';
+
+import { Account } from './entities/account.entity';
 
 @Injectable()
 export class AccountService {
-  // create(createAccountDto: CreateAccountDto) {
-  //   return 'This action adds a new account';
-  // }
-  //
-  // findAll() {
-  //   return `This action returns all account`;
-  // }
-  //
-  // findOne(id: string) {
-  //   return `This action returns a #${id} account`;
-  // }
-  //
-  // update(id: string, updateAccountDto: UpdateAccountDto) {
-  //   return `This action updates a #${id} account`;
-  // }
-  //
-  // remove(id: string) {
-  //   return `This action removes a #${id} account`;
-  // }
+  constructor(
+    @InjectRepository(Account)
+    private accountRepository: Repository<Account>,
+  ) {}
+
+  async create(createAccountDto: CreateAccountDto): Promise<Account> {
+    const newAccount = this.accountRepository.create(createAccountDto);
+
+    try {
+      await this.accountRepository.save(newAccount);
+      return newAccount;
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Failed to save a new account',
+        error,
+      );
+    }
+  }
+
+  async findAllUsersAccounts(userId: string): Promise<Account[]> {
+    return this.accountRepository.find({
+      where: {
+        user: {
+          id: userId,
+        },
+      },
+    });
+  }
+
+  async findOne(id: string): Promise<Account> {
+    const account = await this.accountRepository.findOne({
+      where: {
+        id: id,
+      },
+    });
+    return account;
+  }
+
+  async update(
+    id: string,
+    updateAccountDto: UpdateAccountDto,
+  ): Promise<Account> {
+    await this.accountRepository.update(id, updateAccountDto);
+    const updatedAccount = await this.accountRepository.findOne({
+      where: {
+        id: id,
+      },
+    });
+    if (!updatedAccount) {
+      throw new NotFoundException(`Account with ID "${id}" not found`);
+    }
+    return updatedAccount;
+  }
+
+  async remove(id: string): Promise<void> {
+    const deleteResponse = await this.accountRepository.softDelete(id);
+    if (!deleteResponse.affected) {
+      throw new NotFoundException(`Account with ID "${id}" not found`);
+    }
+  }
+
+  async findAllSoftDeleted(): Promise<Account[]> {
+    return this.accountRepository.find({ withDeleted: true });
+  }
+
+  async hardDeleteOlderThanOneWeek(): Promise<void> {
+    const sevenDaysAgo = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
+    await this.accountRepository.delete({ deletedAt: LessThan(sevenDaysAgo) });
+  }
 }
