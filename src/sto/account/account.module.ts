b83f1f5f6b8ae0c8c9212e0c1df0675851674832
Steve Roberts <steve@shadowcomputers.co.uk>
2024-06-14 20:44:00 +0100

    Fixed missing import

--- src/sto/account/account.module.ts
+++ src/sto/account/account.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Launcher } from '../launcher/entities/launcher.entity';
 import { AccountController } from './account.controller';
 import { AccountService } from './account.service';
 import { Account } from './entities/account.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Account])],
+  imports: [TypeOrmModule.forFeature([Account, Launcher])],
   controllers: [AccountController],
   providers: [AccountService],
   exports: [AccountService],
