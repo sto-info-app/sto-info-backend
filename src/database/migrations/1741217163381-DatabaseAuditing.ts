00cc8c1c4a9d3b168ce955859606748a39dfc5a6
Steve Roberts <steve@shadowcomputers.co.uk>
2025-03-07 18:14:10 +0000

    chore: database table renamed

--- src/database/migrations/1741217163381-DatabaseAuditing.ts
+++ src/database/migrations/1741217163381-DatabaseAuditing.ts
@@ -8,14 +8,12 @@ export class DatabaseAuditing1741217163381 implements MigrationInterface {
       `CREATE TABLE "sto_info_app"."_audit" ("id" SERIAL NOT NULL, "entity" character varying NOT NULL, "action" character varying NOT NULL, "entityId" character varying NOT NULL, "oldValue" json, "newValue" json, "userId" character varying, "ipAddress" character varying, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_0faaf3564b9d0e8a28e83d4bcbe" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
-      `CREATE TABLE "sto_info_app"."_audit_login_attempts" ("id" SERIAL NOT NULL, "email" character varying(255), "ipAddress" character varying, "success" boolean NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_6e3a438f7c8f1a3ce530fec0a12" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "sto_info_app"."_audit_login_attempt" ("id" SERIAL NOT NULL, "email" character varying(255), "ipAddress" character varying, "success" boolean NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_6e3a438f7c8f1a3ce530fec0a12" PRIMARY KEY ("id"))`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `DROP TABLE "sto_info_app"."_audit_login_attempts"`,
-    );
+    await queryRunner.query(`DROP TABLE "sto_info_app"."_audit_login_attempt"`);
     await queryRunner.query(`DROP TABLE "sto_info_app"."_audit"`);
   }
 }
