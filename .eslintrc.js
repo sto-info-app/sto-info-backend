f9cb1301ea793edb32b0439b4eb7c6aab97ade4b
Steve Roberts <steve@shadowcomputers.co.uk>
2025-12-03 03:27:58 +0000

    chore: update ESLint rules and VSCode settings for improved code quality and organization

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,8 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'simple-import-sort/imports': 'error',
-    'simple-import-sort/exports': 'error',
+    // 'simple-import-sort/imports': 'error',
+    // 'no-unused-vars': 'error',
+    // '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
   },
 };
