8e216a2bef3953f3ef6df53d753892947dd7113d
Steve Roberts <steve@shadowcomputers.co.uk>
2025-12-22 07:58:08 +0000

    fix: update user refresh token service test to include mocked update method

--- src/user-refresh-token/user-refresh-token.service.spec.ts
+++ src/user-refresh-token/user-refresh-token.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { UserRefreshTokenEntity } from './entities/user-refresh-token.entity';
 import { UserRefreshTokenService } from './user-refresh-token.service';
 
@@ -13,7 +14,9 @@ describe('UserRefreshTokenService', () => {
         UserRefreshTokenService,
         {
           provide: getRepositoryToken(UserRefreshTokenEntity),
-          useValue: {},
+          useValue: {
+            update: jest.fn(),
+          },
         },
       ],
     }).compile();
