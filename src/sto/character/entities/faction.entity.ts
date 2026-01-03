30dadfc3509ac15493a37f3e06868e96b712a67d
Steve Roberts <steve@shadowcomputers.co.uk>
2026-01-03 17:47:19 +0000

    feat(character): add normalized reference entities for faction, class, species, etc.

---
+++ src/sto/character/entities/faction.entity.ts
@@ -0,0 +1,34 @@
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { CharacterEntity } from './character.entity';
+import type { RecruitTypeEntity } from './recruit-type.entity';
+import type { SpeciesEntity } from './species.entity';
+
+@Entity({ name: 'character_faction' })
+export class FactionEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 100, unique: true })
+  name: string; // Federation, Klingon, TOS Starfleet, etc.
+
+  @OneToMany('CharacterEntity', 'faction')
+  characters: CharacterEntity[];
+
+  @ManyToMany('SpeciesEntity', 'factions')
+  @JoinTable({
+    name: 'faction_species_mapping',
+    joinColumn: { name: 'factionId', referencedColumnName: 'id' },
+    inverseJoinColumn: { name: 'speciesId', referencedColumnName: 'id' },
+  })
+  species: SpeciesEntity[];
+
+  @ManyToMany('RecruitTypeEntity', 'factions')
+  recruitTypes: RecruitTypeEntity[];
+}
