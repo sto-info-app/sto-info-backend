bbffc088ac6f9c538efff4b6ddfb8fb684ee56df
Steve Roberts <steve@shadowcomputers.co.uk>
2026-02-09 02:57:03 +0000

    chore: allows null email in contact requests
    Makes the emailMasked field nullable in the contact_request table.
    This change addresses a scenario where the provided email address are cleaned from the database in automated clean-ups for additional privacy.
    
    Relates to SC-844

--- src/database/migrations/1769401000000-CreateContactRequestTable.ts
+++ src/database/migrations/1769401000000-CreateContactRequestTable.ts
@@ -5,7 +5,7 @@ export class CreateContactRequestTable1769401000000 implements MigrationInterfac
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "sto_info_app"."contact_request" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying(100) NOT NULL, "emailMasked" character varying(320) NOT NULL, "topic" character varying(50) NOT NULL, "message" text NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_contact_request_id" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "sto_info_app"."contact_request" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying(100) NOT NULL, "emailMasked" character varying(320) NULL, "topic" character varying(50) NOT NULL, "message" text NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_contact_request_id" PRIMARY KEY ("id"))`,
     );
   }
 
