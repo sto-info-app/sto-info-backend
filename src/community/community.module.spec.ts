0f06ea858596b8798d530f006cd21aef444bd126
codefactor-io <support@codefactor.io>
2026-08-07 16:51:05 +0000

    [CodeFactor] Apply fixes to commit 4543eca
    [ci skip] [skip ci]

--- src/community/community.module.spec.ts
+++ src/community/community.module.spec.ts
@@ -7,11 +7,14 @@ import { PublicMemberService } from './public-member.service';
 describe('CommunityModule', () => {
   it('declares expected controllers and providers', () => {
     const controllers = Reflect.getMetadata('controllers', CommunityModule) as
-      Array<unknown> | undefined;
+      | Array<unknown>
+      | undefined;
     const providers = Reflect.getMetadata('providers', CommunityModule) as
-      Array<unknown> | undefined;
+      | Array<unknown>
+      | undefined;
     const exportsList = Reflect.getMetadata('exports', CommunityModule) as
-      Array<unknown> | undefined;
+      | Array<unknown>
+      | undefined;
 
     expect(controllers).toContain(CommunityController);
     expect(providers).toEqual(
